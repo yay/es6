d9052e61c2bd09363d48efe5f93bd7429b6d218c
Vitaly Kravchenko <vitalyx@gmail.com>
2017-10-20 22:48:53 +0200

    Closures.

---
+++ src/features/closures.js
@@ -0,0 +1,63 @@
+var n = 5;
+
+function badEcho() {
+    // 'var' defines a variable globally, or locally
+    // to an entire function regardless of block scope.
+    for (var i = 0; i < n; i++) {
+        setTimeout(function () {
+            // Mutable variable is accessed from closure.
+            console.log(i);
+        }, i * 200);
+    }
+}
+
+badEcho();
+// 5
+// 5
+// 5
+// 5
+// 5
+
+function goodEcho() {
+    for (var i = 0; i < n; i++) {
+        (function (i) {
+            setTimeout(function () {
+                console.log(i);
+            }, 1000 + i * 200);
+        })(i);
+    }
+}
+
+goodEcho();
+// 0
+// 1
+// 2
+// 3
+// 4
+
+
+function bestEcho() {
+    // 'let' allows you to declare variables
+    // that are limited in scope to the
+    // block, statement, or expression on which it is used.
+    for (let i = 0; i < n; i++) {
+        setTimeout(function () {
+            console.log(i);
+        }, 2000 + i * 200);
+    }
+    // The example above works as intended because the five instances
+    // of the (anonymous) inner function refer to five different
+    // instances of the variable i.
+}
+
+bestEcho();
+// 0
+// 1
+// 2
+// 3
+// 4
+
+
+// An expression is any valid unit of code that resolves to a value.
+// x = 7   with side effects
+// 3 + 4   without side effects
