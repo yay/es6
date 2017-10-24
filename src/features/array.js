84bf2459fc95ca8790466e4d105ab00353e91970
Vitaly Kravchenko <vitalyx@gmail.com>
2017-10-24 17:55:52 +0200

    Array features.

--- src/features/array.js
+++ src/features/array.js
@@ -4,6 +4,10 @@ if (!Array.of) {
     };
 }
 
+function log(...args) {
+    console.log(...args);
+}
+
 {
     let a = Array.of(5);
     let b = Array.of(1, 2, 3);
@@ -15,4 +19,21 @@ if (!Array.of) {
     // [ 5 ]
     // [ 1, 2, 3 ]
     // [ [ 1, 2 ], [ 3, 4 ] ]
+}
+
+{
+    let r;
+    let a = [1, 2, 3, 4];
+
+    // push - adds to tail (vararg), returns new count
+    r = a.push(9, 10);       log(a, r); // [ 1, 2, 3, 4, 9, 10 ]  6
+
+    // pop - removes a single element from tail and returns it
+    r = a.pop();             log(a, r); // [ 1, 2, 3, 4, 9 ]  10
+
+    // shift - pops a single element head and returns it
+    r = a.shift();           log(a, r); // [ 2, 3, 4, 9 ]  1
+
+    // unshift - pushes to head (vararg), returns new count
+    r = a.unshift(7, 8);     log(a, r); // [ 7, 8, 2, 3, 4, 9 ]  6
 }
