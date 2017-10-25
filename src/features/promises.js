4d3a1ec75078417d91a696151e9a22d650f62b25
Vitaly Kravchenko <vitalyx@gmail.com>
2017-10-25 02:34:05 +0200

    Promises.

--- src/features/promises.js
+++ src/features/promises.js
@@ -1,6 +1,33 @@
 /*
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+The Promise object represents the eventual completion or failure
+of an asynchronous operation, and its resulting value.
+
+let promise = doSomething();
+promise.then(successCallback, failureCallback);
+
+or
+
+promise
+    .then(successCallback)
+    .catch(failureCallback)
+
+catch(failureCallback) is short for then(null, failureCallback)
+
+
+Unlike old-style passed-in callbacks, a promise comes with some guarantees:
+
+- Callbacks will never be called before the completion of the current run
+  of the JavaScript event loop
+
+- Callbacks added with .then even after the success or failure of the
+  asynchronous operation, will be called
+
+- Multiple callbacks may be added by calling .then several times,
+  to be executed independently in insertion order
+
+The 'then' function returns a new promise (different from the original)
+returned by the then's successCallback, failureCallback or generated automatically.
 
 */
 
@@ -8,7 +35,7 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
     function timeout(duration = 0) {
         return new Promise((resolve, reject) => {
             setTimeout(resolve, duration);
-        })
+        });
     }
 
     let p = timeout(1000).then(() => {
@@ -21,3 +48,29 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
         return Promise.all([timeout(100), timeout(200)]);
     });
 }
+
+function inSequence(promises) {
+    return promises.reduce(
+        (prev, next) => prev.then(next), Promise.resolve()
+    );
+}
+
+new Promise((resolve, reject) => resolve('success'))
+    .then(v => console.log(1, v))   // 1 'success'
+    .then(v => console.log(2, v))   // 2 undefined
+    .then(v => console.log(3, v))   // 3 undefined
+    .then(() => {
+        throw new Error('failure');
+    })
+    .catch(err => console.log(err))       // Error: failure
+    .then(() => console.log('Carry on')); // Carry on
+
+// Basically, a promise chain stops if there's an exception,
+// looking down the chain for catch handlers instead.
+
+new Promise((resolve, reject) => reject('failure'))
+    .then(v => console.log(v))     // doesn't run
+    .then(() => console.log('b'))  // doesn't run
+    .then(() => console.log('c'))  // doesn't run
+    .catch(err => console.log('Catch:', err))         // Catch: failure
+    .then(() => console.log('Continue after catch')); // Continue after catch
