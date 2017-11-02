b8ddd3f05a328e30dabd5ca20efd23869ac798f8
Vitaly Kravchenko <vitalyx@gmail.com>
2017-11-02 13:47:55 +0100

    Tweaks.

--- src/leetcode/binaryTreeLevelOrderTraversal.js
+++ src/leetcode/binaryTreeLevelOrderTraversal.js
@@ -27,17 +27,33 @@ root.right = new TreeNode(20);
 root.right.right = new TreeNode(7);
 root.right.left = new TreeNode(15);
 
-function levelOrder(root, levels, level) {
-    if (root !== null) {
+function levelOrder(root, levels, level) {  // modified in-order
+    if (root) {
         levelOrder(root.left, levels, level + 1);
-        console.log(root.val);
         let arr = levels[level] || (levels[level] = []);
         arr.push(root.val);
         levelOrder(root.right, levels, level + 1);
     }
 }
 
-let levels = [];
-levelOrder(root, levels, 0);
+function levelOrder2(root, levels, level) { // modified post-order
+    if (root) {
+        levelOrder2(root.left, levels, level + 1);
+        levelOrder2(root.right, levels, level + 1);
+        let arr = levels[level] || (levels[level] = []);
+        arr.push(root.val);
 
-console.log(levels);
+    }
+}
+
+{
+    let levels = [];
+    levelOrder(root, levels, 0);
+    console.log(levels);
+}
+
+{
+    let levels = [];
+    levelOrder2(root, levels, 0);
+    console.log(levels);
+}
