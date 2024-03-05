d5fd01559075124057c2e97b6ac2b9e30b5804fd
Vitalii Kravchenko <vitalyx@gmail.com>
2024-03-05 20:14:58 +0000

    Move from Webpack to Vite.

--- src/algorithm/binarySearch.js
+++ src/algorithm/binarySearch.js
@@ -3,35 +3,34 @@ import { removeDuplicates } from "../helper/dedupe";
 
 let arr = randomSortedNumbers(30, 0, 1000);
 removeDuplicates(arr);
-console.log(arr.join(' '));
+console.log(arr.join(" "));
 
 function binarySearch(array, target) {
-    let lo = 0;
-    let hi = array.length - 1;
+  let lo = 0;
+  let hi = array.length - 1;
 
-    console.log(`Binary search steps (index : value) for array of ${hi + 1} elements:`);
+  console.log(
+    `Binary search steps (index : value) for array of ${hi + 1} elements:`
+  );
 
-    while (lo <= hi) {
-        let iMid = Math.floor((lo + hi) / 2);
-        let vMid = array[iMid];
+  while (lo <= hi) {
+    let iMid = Math.floor((lo + hi) / 2);
+    let vMid = array[iMid];
 
-        console.log(`${iMid} : ${vMid}`);
+    console.log(`${iMid} : ${vMid}`);
 
-        if (target === vMid)
-            return iMid;
-        else if (target < vMid)
-            hi = iMid - 1;
-        else
-            lo = iMid + 1;
-    }
+    if (target === vMid) return iMid;
+    else if (target < vMid) hi = iMid - 1;
+    else lo = iMid + 1;
+  }
 
-    return -1;
+  return -1;
 }
 
 let results = [
-    binarySearch(arr, arr[5]),
-    binarySearch(arr, arr[8]),
-    binarySearch(arr, arr[11]),
+  binarySearch(arr, arr[5]),
+  binarySearch(arr, arr[8]),
+  binarySearch(arr, arr[11]),
 ];
 
 console.log();
--- src/algorithm/binarySearchTree.js
+++ src/algorithm/binarySearchTree.js
@@ -1,147 +1,147 @@
 export class BNode {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 export class BTree {
-    constructor() {
-        this.root = null;
-    }
+  constructor() {
+    this.root = null;
+  }
 
-    find(data) {
-        let it = this.root;
+  find(data) {
+    let it = this.root;
 
-        while (it !== null) {
-            if (it.data === data) {
-                return true;
-            } else {
-                it = it.data < data ? it.right : it.left;
-            }
-        }
-        return false;
-    }
-
-    _findR(data, root) {
-        if (root) {
-            if (root.data === data) {
-                return true;
-            } else {
-                let next = root.data < data ? root.right : root.left;
-                return this._findR(data, next);
-            }
-        }
-        return false;
-    }
-
-    findRecursively(data) {
-        return this._findR(data, this.root);
-    }
-
-    insert(data) {
-        if (this.root === null) {
-            this.root = new BNode(data);
-        } else {
-            let it = this.root;
-
-            for (;;) {
-                if (it.data === data) {
-                    return false;
-                } else {
-                    if (it.data < data) {
-                        if (it.right === null) {
-                            it.right = new BNode(data);
-                            break;
-                        }
-                        it = it.right;
-                    } else {
-                        if (it.left === null) {
-                            it.left = new BNode(data);
-                            break;
-                        }
-                        it = it.left;
-                    }
-                }
-            }
-        }
-
+    while (it !== null) {
+      if (it.data === data) {
         return true;
+      } else {
+        it = it.data < data ? it.right : it.left;
+      }
     }
+    return false;
+  }
 
-    remove(data) {
-        if (this.root !== null) {
-            let head = new BNode(null);
-            let it = head;
-            let parent = null;
-            let found = null;
+  _findR(data, root) {
+    if (root) {
+      if (root.data === data) {
+        return true;
+      } else {
+        let next = root.data < data ? root.right : root.left;
+        return this._findR(data, next);
+      }
+    }
+    return false;
+  }
 
-            let child = it.right = this.root;
+  findRecursively(data) {
+    return this._findR(data, this.root);
+  }
 
-            while (child !== null) {
-                parent = it;
-                it = child;
+  insert(data) {
+    if (this.root === null) {
+      this.root = new BNode(data);
+    } else {
+      let it = this.root;
 
-                if (it.data === data) {
-                    found = it;
-                }
-
-                // Use '<=' so that we go right once from the 'found' node
-                // then all the way left, till 'it' is an in-order successor
-                // (left child is null), at which point we break the loop.
-                child = it.data <= data ? it.right : it.left;
+      for (;;) {
+        if (it.data === data) {
+          return false;
+        } else {
+          if (it.data < data) {
+            if (it.right === null) {
+              it.right = new BNode(data);
+              break;
             }
-
-            if (found !== null) {
-                found.data = it.data;
-
-                let child = it.left === null ? it.right : it.left;
-
-                if (parent.right === it) {
-                    parent.right = child;
-                } else {
-                    parent.left = child;
-                }
+            it = it.right;
+          } else {
+            if (it.left === null) {
+              it.left = new BNode(data);
+              break;
             }
-
-            this.root = head.right;
+            it = it.left;
+          }
         }
+      }
     }
 
-    _preOrder(root) {
-        if (root !== null) {
-            console.log(root.data);
-            this._preOrder(root.left);
-            this._preOrder(root.right);
+    return true;
+  }
+
+  remove(data) {
+    if (this.root !== null) {
+      let head = new BNode(null);
+      let it = head;
+      let parent = null;
+      let found = null;
+
+      let child = (it.right = this.root);
+
+      while (child !== null) {
+        parent = it;
+        it = child;
+
+        if (it.data === data) {
+          found = it;
         }
-    }
 
-    _inOrder(root) {
-        if (root !== null) {
-            this._inOrder(root.left);
-            console.log(root.data);
-            this._inOrder(root.right);
+        // Use '<=' so that we go right once from the 'found' node
+        // then all the way left, till 'it' is an in-order successor
+        // (left child is null), at which point we break the loop.
+        child = it.data <= data ? it.right : it.left;
+      }
+
+      if (found !== null) {
+        found.data = it.data;
+
+        let child = it.left === null ? it.right : it.left;
+
+        if (parent.right === it) {
+          parent.right = child;
+        } else {
+          parent.left = child;
         }
-    }
+      }
 
-    _postOrder(root) {
-        if (root !== null) {
-            this._postOrder(root.left);
-            this._postOrder(root.right);
-            console.log(root.data);
-        }
+      this.root = head.right;
     }
+  }
 
-    preOrder() {
-        this._preOrder(this.root);
+  _preOrder(root) {
+    if (root !== null) {
+      console.log(root.data);
+      this._preOrder(root.left);
+      this._preOrder(root.right);
     }
+  }
 
-    inOrder() {
-        this._inOrder(this.root);
+  _inOrder(root) {
+    if (root !== null) {
+      this._inOrder(root.left);
+      console.log(root.data);
+      this._inOrder(root.right);
     }
+  }
 
-    postOrder() {
-        this._postOrder(this.root);
+  _postOrder(root) {
+    if (root !== null) {
+      this._postOrder(root.left);
+      this._postOrder(root.right);
+      console.log(root.data);
     }
+  }
+
+  preOrder() {
+    this._preOrder(this.root);
+  }
+
+  inOrder() {
+    this._inOrder(this.root);
+  }
+
+  postOrder() {
+    this._postOrder(this.root);
+  }
 }
